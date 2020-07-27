1bfce83561f6306403e4d3a093e289c00214a965
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-07-27 11:45:58 -0300

    first commit

---
+++ src/test/java/com/jfb/meudinheiro/MeuDinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfb.meudinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
