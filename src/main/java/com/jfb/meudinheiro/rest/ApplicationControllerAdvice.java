f16213676398f54c501629ea7ca5f925ea10667b
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-07-30 02:21:17 -0300

    Padronizando o retorno do erro.(OK)

---
+++ src/main/java/com/jfb/meudinheiro/rest/ApplicationControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.jfb.meudinheiro.rest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jfb.meudinheiro.rest.exceptions.ApiErrors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationErrors(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> messages =  bindingResult
+            .getAllErrors()
+            .stream().map(objectError -> objectError.getDefaultMessage())
+            .collect(Collectors.toList());
+        return new ApiErrors(messages);
+    }
+}
