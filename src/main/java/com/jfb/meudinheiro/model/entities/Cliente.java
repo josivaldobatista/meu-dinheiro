5232a97df4a986bc6873ffe30500f89ffe20f99a
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-07 18:28:28 -0300

    Mais alguns ajustes: Liberando o Cors e e DataCadastro dando null.

--- src/main/java/com/jfb/meudinheiro/model/entities/Cliente.java
+++ src/main/java/com/jfb/meudinheiro/model/entities/Cliente.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -42,5 +43,10 @@ public class Cliente {
     @Column(name = "data_cadastro", updatable = false)
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDate.now());
+    }
 
 }
