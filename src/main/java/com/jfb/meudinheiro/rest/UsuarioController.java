2a4c8290fc50e2610650274ca50e875e40166db1
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-16 21:30:25 -0300

    Configurando SpringSecurity e testes em memoria.(Estudo)

---
+++ src/main/java/com/jfb/meudinheiro/rest/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.jfb.meudinheiro.rest;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jfb.meudinheiro.model.entities.Usuario;
+import com.jfb.meudinheiro.model.repositories.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+	private final UsuarioRepository usuarioRepository;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void salvar(@RequestBody @Valid Usuario usuario) {
+		usuarioRepository.save(usuario);
+	}
+}
